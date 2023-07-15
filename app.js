c569ec041fc2335a0affd873b6916a05ae45026b
venkatesh0024 <138753506+venkatesh0024@users.noreply.github.com>
2023-07-15 12:08:21 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+var outputScreen = document.getElementById("display");
+function Display(num) {
+    outputScreen.value += num;
+}
+function calculate() {
+    try {
+        outputScreen.value = eval(outputScreen.value);
+    } catch (err) {
+        alert("Invalid");
+    }
+}
+function AC() {
+    outputScreen.value = " ";
+}
+function Delete() {
+    outputScreen.value = outputScreen.value.slice(0, -1);
+}
